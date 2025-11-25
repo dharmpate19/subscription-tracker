4e61fb92b96646767834bc743d174a52ef6540d7
Dharm Patel <pdharm774@gmail.com>
2025-11-26 00:29:34 +0530

    Create routes for auth, user and subscription

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 import express from 'express';
 import {PORT} from './config/env.js';
+import authRouter from './routes/auth.routes.js';
+import userRouter from './routes/user.routes.js';
+import subscriptionRouter from './routes/subscription.routes.js';
 
 
 const app = express();
@@ -7,6 +10,10 @@ const app = express();
 app.get('/', (req,res) =>{
     res.send("Welcome to teh subscription tracker API")
 })
+
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/user', userRouter);
+app.use('/api/v1/subscription', subscriptionRouter);
 
 app.listen(PORT, () => console.log(`Server running on port http://localhost:${PORT}`));
 
