65e91840844eaac329d0b4ea9f565b9b5608c3fe
Dharm Patel <pdharm774@gmail.com>
2025-12-07 13:07:01 +0530

    add correct name in models

--- models/subscription.model.js
+++ models/subscription.model.js
@@ -35,7 +35,7 @@ const subscriptionSchema = new mongoose.Schema({
     }, 
     status : {
         type : String,
-        enum : ['active', 'calcelled', 'expired'],
+        enum : ['active', 'cancelled', 'expired'],
         default : 'active'
     }, 
     startDate: {
