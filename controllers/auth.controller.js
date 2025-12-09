a800f8aba421b1cf02c87a4435fbfce6f3440eca
Dharm Patel <pdharm774@gmail.com>
2025-12-10 00:07:24 +0530

    update auth for user to fetch data

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -30,7 +30,7 @@ export const signUp = async (req, res, next) => {
 
        const newUsers = await User.create([{name, email, password : hashedPassword}], {session});
 
-       const token = jwt.sign({UserId : newUsers[0]._id},  JWT_SECRET, {expiresIn : JWT_EXPIRES_IN});
+       const token = jwt.sign({userId : newUsers[0]._id},  JWT_SECRET, {expiresIn : JWT_EXPIRES_IN});
        await session.commitTransaction();
        session.endSession();
 
@@ -69,7 +69,7 @@ export const signIn = async (req, res, next) => {
                 throw error;
             }
 
-            const token = jwt.sign({UserId : user._id},  JWT_SECRET, {expiresIn : JWT_EXPIRES_IN});
+            const token = jwt.sign({userId : user._id},  JWT_SECRET, {expiresIn : JWT_EXPIRES_IN});
             
             res.status(200).json({
                 success : true,
