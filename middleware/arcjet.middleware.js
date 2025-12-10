8dcce1d22f4b8434ba9e631fca1523425c8e29ea
Dharm Patel <pdharm774@gmail.com>
2025-12-10 23:53:08 +0530

    change as the the rabbit suggestion

--- middleware/arcjet.middleware.js
+++ middleware/arcjet.middleware.js
@@ -5,7 +5,7 @@ const arcjetMiddleware = async (req, res, next) => {
         const decision = await aj.protect(req, {requested : 1});
 
         if(decision.isDenied()){
-            if(decision.reason.isRateLimit())return res.status(409).json({error :'Rate Limit Exceeded'});
+            if(decision.reason.isRateLimit())return res.status(429).json({error :'Rate Limit Exceeded'});
             if(decision.reason.isBot()) return res.status(403).json({error : 'Bot Detected'});
 
             return res.status(403).json({error : 'Access Denied'});
