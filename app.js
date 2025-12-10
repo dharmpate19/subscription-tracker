cc3f6f573d950470b719e5f4897dbb441d3112be
Dharm Patel <pdharm774@gmail.com>
2025-12-10 23:45:05 +0530

    Create rate limit

--- app.js
+++ app.js
@@ -7,14 +7,17 @@ import subscriptionRouter from './routes/subscription.routes.js';
 import connectToDatabase from './database/mongodb.js';
 import errorMiddleware from './middleware/error.middleware.js';
 import cookieParser from 'cookie-parser';
+import arcjetMiddleware from './middleware/arcjet.middleware.js';
 
 
 
 const app = express();
+app.set("trust proxy", true);
 
 app.use(express.json());
 app.use(express.urlencoded({extended : false}));
-app.use(cookieParser())
+app.use(cookieParser());
+app.use(arcjetMiddleware)
 
 app.get('/', (req,res) =>{
     res.send("Welcome to teh subscription tracker API")
